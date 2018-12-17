b4a53f1132a0ca0f66d00e7b9502dd8cd0059991
Justus Ray <justusmray@gmail.com>
2018-12-17 12:09:56 -0700

    This is the last version which completed all the tests.

--- assessment.js
+++ assessment.js
@@ -22,7 +22,7 @@ var trueFaveNum = faveNum == 7;
 // #6 Create a new variable called notHakeem. Using the previously created myName variable, test that your name DOES NOT equal the string 'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'
 
 // CODE HERE
-var notHakeem = (myName == "Hakeem Olajuwon");
+var notHakeem = myName != "Hakeem Olajuwon";
 // #7 Create an if statement that checks the age variable below. If age is greater than 16, then set canDrive to true otherwise set canDrive to false. The canDrive variable will be set by the unit test so do not adjust it.
 var age = 18;
 var canDrive;
@@ -42,10 +42,10 @@ var colorResponse;
 
 var responseCreator = function(param){
   switch (param){
-    case green: 
+    case 'green': 
     colorResponse = "Green is okay";
     break;
-    case red:
+    case 'red':
     colorResponse = "I heart red";
     break;
     default:
@@ -57,7 +57,15 @@ var responseCreator = function(param){
 // #9 Create a function called 'sum' that takes in one parameter and returns the sum of that parameter added it itself.
 
 // CODE HERE
+function sum(param){
+  
 
+  return param += param;
+
+}
 // #10 Create a function called 'greeter' that takes in two parameters, name and number. Have 'greeter' return a string that says something like this 'Hi! I am Bryan and I am 28 years old" but with the appropriate values..
 
 // CODE HERE
+function greeter(name, age){
+  return "Hi! I am " + name + " and I am " + age + " years old"
+}
